7ea8a43459ff2327098d747b9f55fd26f2108e7b
Donovan <donovan6925@gmail.com>
2017-12-01 18:43:43 -0500

    added custom header

---
+++ src/HeaderMenu.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react'
+import { Dropdown, Menu } from 'semantic-ui-react'
+
+export default class HeaderMenu extends Component {
+  state = {}
+
+  handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+
+  render() {
+    const { activeItem } = this.state
+
+    return (
+      <Menu inverted>
+        <Menu.Item header>The Game</Menu.Item>
+        <Menu.Item name='market' active={activeItem === 'market'} onClick={this.handleItemClick} />
+        <Menu.Item name='Stats' active={activeItem === 'Stats'} onClick={this.handleItemClick} />
+        <Menu.Item name='tutorial' active={activeItem === 'tutorial'} onClick={this.handleItemClick} />
+
+        <Menu.Menu position='right'>
+          <Dropdown item text='Language'>
+            <Dropdown.Menu>
+              <Dropdown.Item>English</Dropdown.Item>
+              <Dropdown.Item>Russian</Dropdown.Item>
+              <Dropdown.Item>Spanish</Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+
+          <Menu.Item name="account" active={activeItem === 'account'} onClick={this.handleItemClick} />
+        </Menu.Menu>
+      </Menu>
+    )
+  }
+}
---
+++ src/Layout.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+import HeaderMenu from "./HeaderMenu";
+
+import './App.css';
+
+class Layout extends Component {
+  render() {
+    return (
+      <div className="App">
+        <HeaderMenu />
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default Layout;
