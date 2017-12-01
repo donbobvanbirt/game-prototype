41b4e4a1ee9d2f1c9680f4b9e8457588d824e83d
Donovan <donovan6925@gmail.com>
2017-12-01 18:26:27 -0500

    added semantic ui

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+
 import App from './App';
+
+import './index.css';
+import 'semantic-ui-css/semantic.min.css';
+
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
