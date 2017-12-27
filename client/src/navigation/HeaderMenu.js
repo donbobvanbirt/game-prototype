dee7f10f727d981581cf744d9e38568ee5fe20eb
Donovan <donovan6925@gmail.com>
2017-12-27 13:19:48 -0500

    made header responsive

--- client/src/navigation/HeaderMenu.js
+++ client/src/navigation/HeaderMenu.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import { Menu, Icon } from 'semantic-ui-react';
+import { Menu, Icon, Dropdown } from 'semantic-ui-react';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
+import MediaQuery from 'react-responsive';
 
 const CustomMenu = styled(Menu)`
   border-radius: 0 !important;
@@ -22,10 +23,22 @@ export default class HeaderMenu extends Component {
 
     return (
       <CustomMenu inverted>
-        <Menu.Item header>The Game</Menu.Item>
-        <Menu.Item name="market" active={activeItem === 'market'} onClick={this.handleItemClick} />
-        <Menu.Item name="Stats" active={activeItem === 'Stats'} onClick={this.handleItemClick} />
-        <Menu.Item name="tutorial" active={activeItem === 'tutorial'} onClick={this.handleItemClick} />
+        <Menu.Item header>Zahrio</Menu.Item>
+        <MediaQuery minWidth={401}>
+          <Menu.Item name="market" active={activeItem === 'market'} onClick={this.handleItemClick} />
+          <Menu.Item name="Stats" active={activeItem === 'Stats'} onClick={this.handleItemClick} />
+          <Menu.Item name="tutorial" active={activeItem === 'tutorial'} onClick={this.handleItemClick} />
+        </MediaQuery>
+
+        <MediaQuery maxWidth={400}>
+          <Dropdown item>
+            <Dropdown.Menu>
+              <Dropdown.Item>Market</Dropdown.Item>
+              <Dropdown.Item>Stats</Dropdown.Item>
+              <Dropdown.Item>Tutorial</Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+        </MediaQuery>
 
         <Menu.Menu position="right">
           {!!toggleSideMenu &&
