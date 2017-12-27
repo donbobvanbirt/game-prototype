5febb3534653fe33c8258a745f40cf7fabe81f77
Donovan <donovan6925@gmail.com>
2017-12-27 09:58:16 -0500

    updated join function

--- server/index.js
+++ server/index.js
@@ -25,7 +25,7 @@ app.use(express.static(path.join(__dirname, 'client/build')));
 app.use('/api', require('./routes/api'));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname+'../client/build/index.html'));
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
 const port = process.env.PORT || 5000;
